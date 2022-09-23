64af8ae7662b4f4c622dc46f2de3915070bc6de6
moliman33 <mrodriguezm777@gmail.com>
2022-09-22 18:10:40 -0600

    primeros archivos

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Descripcion de mi pagina web">
+    <meta name="robots" content="noindex">
+    <link rel="stylesheet" href="publica/css/estilos.css">
+
+
+    <title>Mi pagina web</title>
+</head>
+<body class="grid-contenedor">
+    <header class="header">
+        <h1>Este es mi logo</h1>
+    </header>
+    <nav class="nav-bar">
+        Este es el menu de navegacion
+    </nav>
+    
+    <main class="main">
+        <section class="box-ppal">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam est quae fugiat ipsam, dolores ex fuga, tenetur soluta dicta ab numquam dolorum temporibus consequuntur dignissimos consequatur maiores recusandae quos enim!
+        </section>
+    </main>
+    <aside class="aside">
+        <h2>aside</h2>
+    </aside>
+    <footer class="footer">
+        <h3>Esto es el footer</h3>
+    </footer>
+    <div class="polics-cookies">
+
+    </div>
+</body>
+</html>

cf0fc1a342e7bc62c90e498fbdf57b73ff2c8eef
moliman <mrodriguezm777@gmail.com>
2023-03-07 23:57:04 -0600

    Menu responsive parte2

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -11,27 +12,108 @@
 
     <title>Mi pagina web</title>
 </head>
+
 <body class="grid-contenedor">
     <header class="header">
         <h1>Este es mi logo</h1>
     </header>
     <nav class="nav-bar">
-        Este es el menu de navegacion
+        <div class="box-nav">
+            <input type="checkbox" id="menu">
+            <label for="menu">
+                <span class="burger1"></span>
+                <span class="burger2"></span>
+                <span class="burger3"></span>
+            </label>
+
+
+
+            <ul>
+                <li>
+                    <a href="/">
+
+                        <svg class="icon-home" viewBox="0 0 64 64">
+                            <use xlink:href="publica/img/iconos.svg#home">
+                        </svg>
+                    </a>
+                </li>
+
+                <li><a href="">Contactanos</a></li>
+                <li class="-usuario"><a href="#">Iniciar Sesion</a></li>
+                <li><a href="Registro">Registrarse </a></li>
+
+            </ul>
+        </div>
+
+
+
     </nav>
-    
+
     <main class="main">
         <section class="box-ppal">
+
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam est quae fugiat ipsam, dolores ex fuga, tenetur soluta dicta ab numquam dolorum temporibus consequuntur dignissimos consequatur maiores recusandae quos enim!
         </section>
     </main>
     <aside class="aside">
-        <h2>aside</h2>
+        <section class="box-aside">
+            <div class="margen">
+            <h2>aside</h2>
+            </div>
+            
+        </section>
+
+
     </aside>
     <footer class="footer">
-        <h3>Esto es el footer</h3>
+        <div class="box-footer">
+            <div class="col b-left">
+                <p class="p-footer">
+                    Acerca de
+                </p>
+            </div>
+
+            <div class="col b-left">
+                <p class="p footer">
+                    Servicio al Cliente
+                </p>
+                <p>
+                    <a href="">Preguntas frecuentes</a>
+                    <a href="">Contactanos</a>
+                    <a href="">Nuestra tienda</a>
+                </p>
+            </div>
+
+            <div class="col">
+                <p class="p-footer">
+                    Politicas
+                </p>
+                <p>
+                    <a href="">Politicas de privacidad</a>
+                    <a href="">Aviso Legal</a>
+                    <a href="">Politica de cookie</a>
+                </p>
+            </div>
+
+        </div>
+
+            <div class="copy">
+                <small>Manuel Molina 2023</small>
+                <small>www.mitiendaonline.com</small>
+            </div>
+            
+        
+        
     </footer>
     <div class="polics-cookies">
 
     </div>
 </body>
+
+
+
+
+
+
+
 </html>
